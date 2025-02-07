8db8c038161ecd32552f70a9190a1fe87667c7df
Wandelson <wandelson.bezer@gmail.com>
2025-02-07 18:14:28 -0300

    helloword

---
+++ WebApplication/WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+
+        private static readonly string[] usersDb = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+
+        [HttpGet]
+        public ActionResult Get(string name )
+        {
+
+            try
+            {
+                var user = usersDb.Where(x => x.Contains(name)).Last();
+                return Ok(user);
+            }catch(Exception ex)
+            {
+                //log 
+                return NoContent();
+            }
+        }
+    }
+}
